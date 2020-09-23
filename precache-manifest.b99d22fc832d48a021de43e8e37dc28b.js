efbff2c4142e05a1399a24597bcf831248b6f4fc
zhangyi999 <zhang_btc@outlook.com>
2020-09-24 02:41:33 +0800

    first commit

---
+++ precache-manifest.b99d22fc832d48a021de43e8e37dc28b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e657b37c6fbb076b5ad0e8e094739ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21783a00deacaa95103d",
+    "url": "/static/css/main.9bc6d313.chunk.css"
+  },
+  {
+    "revision": "cfdf26bf71179df4428a",
+    "url": "/static/js/0.930a0949.chunk.js"
+  },
+  {
+    "revision": "d0a0fafcf389e2d3fd75",
+    "url": "/static/js/10.b3dd02ab.chunk.js"
+  },
+  {
+    "revision": "b33b64799a02755e4492",
+    "url": "/static/js/3.5802131e.chunk.js"
+  },
+  {
+    "revision": "ca5c4c691ea0cbca780abf4100c36ae5",
+    "url": "/static/js/3.5802131e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5a86eb76055011cc88",
+    "url": "/static/js/4.7aa713be.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "/static/js/4.7aa713be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4f5eafddc5c208f452",
+    "url": "/static/js/5.5c8a571f.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/5.5c8a571f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837db2d46e5ab5a27a12",
+    "url": "/static/js/6.fcf56db3.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/6.fcf56db3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e88e751a9deaf0597c",
+    "url": "/static/js/7.9dd1da13.chunk.js"
+  },
+  {
+    "revision": "1b2c983f2a98befb30cf",
+    "url": "/static/js/8.b7fc5cc2.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/8.b7fc5cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9ffc547dea0719190f",
+    "url": "/static/js/9.0cd46df0.chunk.js"
+  },
+  {
+    "revision": "21783a00deacaa95103d",
+    "url": "/static/js/main.86735cd9.chunk.js"
+  },
+  {
+    "revision": "01918ffc9f2e9ff07e80",
+    "url": "/static/js/runtime-main.a0d9c9f9.js"
+  },
+  {
+    "revision": "ec78e8e86a08b2278fa9baf09388d697",
+    "url": "/static/media/chef.ec78e8e8.png"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
